f52c6b8364831ae2437ca00ce39f90896d4ff824
priyanka <priyanka@leapfunder.nl>
2025-02-17 17:42:19 +0530

    Migration merge

--- db/migrate/20250217090712_add_column_to_user.rb
+++ db/migrate/20250217090712_add_column_to_user.rb
@@ -1,5 +1,6 @@
 class AddColumnToUser < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :role, :string, default: "member"
+    add_column :users, :name, :string
   end
 end
--- db/migrate/20250217110400_add_column_name_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnNameToUser < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :name, :string
-  end
-end
